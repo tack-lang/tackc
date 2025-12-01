1771cb677f29aeb55b0489c0b22cb950ddfd543e
DDAN <55860416+DDAN-17@users.noreply.github.com>
2025-12-01 12:57:05 -0600

    Refactor comphash

--- crates/tackc_meta/src/bin/hash.rs
+++ crates/tackc_meta/src/bin/hash.rs
@@ -1,54 +1,9 @@
 //! Written by AI, verified by human.
 
 use sha2::{Digest, Sha256};
-use std::{
-    env, fs, io,
-    path::{Path, PathBuf},
-};
+use std::{fs, path::Path};
+use tackc_meta::chdir_to_tack_root;
 use walkdir::WalkDir;
-
-/// Change current working directory to the Tack project root.
-///
-/// Looks upward from the current directory until it finds a directory
-/// containing `crates/tackc_*`.
-pub fn chdir_to_tack_root() -> io::Result<PathBuf> {
-    let mut dir = env::current_dir()?;
-
-    loop {
-        if is_tack_root(&dir) {
-            env::set_current_dir(&dir)?;
-            return Ok(dir);
-        }
-
-        // If we hit filesystem root and didn't find it -> fail.
-        if !dir.pop() {
-            return Err(io::Error::new(
-                io::ErrorKind::NotFound,
-                "Could not find Tack root directory",
-            ));
-        }
-    }
-}
-
-fn is_tack_root(dir: &Path) -> bool {
-    let crates = dir.join("crates");
-    if !crates.is_dir() {
-        return false;
-    }
-
-    // Look for something like crates/tackc_*
-    if let Ok(entries) = fs::read_dir(crates) {
-        for entry in entries.flatten() {
-            if let Some(name) = entry.file_name().to_str()
-                && name.starts_with("tackc_")
-            {
-                return true;
-            }
-        }
-    }
-
-    false
-}
 
 fn main() {
     chdir_to_tack_root().expect("Failed to setup current directory!");
