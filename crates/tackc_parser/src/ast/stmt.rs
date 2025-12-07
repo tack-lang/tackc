1514c556dc6111c61ce6d62f52076466d9f11f5d
DDAN <55860416+DDAN-17@users.noreply.github.com>
2025-12-06 21:30:30 -0600

    Allow item as statement Remove parsing for `Statement`/`ExpressionStatement`

--- crates/tackc_parser/src/ast/stmt.rs
+++ crates/tackc_parser/src/ast/stmt.rs
@@ -4,7 +4,7 @@ use tackc_span::Span;
 
 use crate::{
     Parser,
-    ast::{AstNode, Expression, Symbol},
+    ast::{AstNode, Expression, Item, Symbol},
     error::{DiagResult, Result},
 };
 
@@ -23,6 +23,9 @@ impl AstNode for StatementOrExpression {
         let tok = p.expect_peek_token(None)?;
         #[allow(clippy::single_match_else)]
         match tok.kind {
+            TokenKind::Const => p.parse::<Item>(recursion + 1)
+                .map(Statement::Item)
+                .map(StatementOrExpression::Statement),
             TokenKind::Let => p
                 .parse::<LetStatement>(recursion + 1)
                 .map(Statement::LetStatement)
@@ -65,35 +68,23 @@ impl AstNode for StatementOrExpression {
 pub enum Statement {
     ExpressionStatement(ExpressionStatement),
     LetStatement(LetStatement),
+    Item(Item),
 }
 
-impl AstNode for Statement {
-    fn parse<I>(p: &mut Parser<I>, recursion: u32) -> Result<Self>
-    where
-        I: Iterator<Item = Token> + Clone,
-    {
-        let tok = p.expect_peek_token(None)?;
-        match tok.kind {
-            TokenKind::Let => p
-                .parse::<LetStatement>(recursion + 1)
-                .map(Statement::LetStatement),
-            _ => p
-                .parse::<ExpressionStatement>(recursion + 1)
-                .map(Statement::ExpressionStatement),
-        }
-    }
-
-    fn span(&self) -> Span {
+impl Statement {
+    pub fn span(&self) -> Span {
         match self {
             Statement::ExpressionStatement(stmt) => stmt.span(),
             Statement::LetStatement(stmt) => stmt.span(),
+            Statement::Item(item) => item.span(),
         }
     }
 
-    fn display(&self, global: &Global) -> String {
+    pub fn display(&self, global: &Global) -> String {
         match self {
             Statement::ExpressionStatement(stmt) => stmt.display(global),
             Statement::LetStatement(stmt) => stmt.display(global),
+            Statement::Item(item) => item.display(global),
         }
     }
 }
@@ -109,25 +100,12 @@ impl ExpressionStatement {
     fn new(inner: Expression, span: Span) -> Self {
         ExpressionStatement { inner, span }
     }
-}
 
-impl AstNode for ExpressionStatement {
-    fn parse<I>(p: &mut Parser<I>, recursion: u32) -> Result<Self>
-    where
-        I: Iterator<Item = tackc_lexer::Token> + Clone,
-    {
-        let expr = p.parse::<Expression>(recursion + 1)?;
-        let semi = p.expect_token_kind(Some("';'"), token_kind!(TokenKind::Semicolon))?;
-        let span = Span::new_from(expr.span.start, semi.span.end);
-
-        Ok(ExpressionStatement::new(expr, span))
-    }
-
-    fn span(&self) -> Span {
+    pub fn span(&self) -> Span {
         self.span
     }
 
-    fn display(&self, global: &Global) -> String {
+    pub fn display(&self, global: &Global) -> String {
         self.inner.display(global) + ";"
     }
 }
@@ -217,6 +195,12 @@ fn run_stmt_test(path: &Path) {
         .unwrap_or_else(|_| panic!("Failed to open file {}", path.display()));
     let lexer = Lexer::new(&src, &global).consume_reporter(drop);
     let mut p = Parser::new(lexer);
-    let expr = Statement::parse(&mut p, 0).expected("statement");
+    let expr = StatementOrExpression::parse(&mut p, 0).expected("statement").map(|stmt_or_expr| {
+        if let StatementOrExpression::Statement(stmt) = stmt_or_expr {
+            stmt
+        } else {
+            panic!("expected statement, found expression");
+        }
+    });
     insta::assert_ron_snapshot!(expr);
 }
