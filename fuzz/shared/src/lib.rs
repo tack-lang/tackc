2fcf92e3907a634d8a4d1d5409fded9f9d63cf24
DDAN <55860416+DDAN-17@users.noreply.github.com>
2025-12-10 20:45:42 -0600

    Fix fuzzing

--- fuzz/shared/src/lib.rs
+++ fuzz/shared/src/lib.rs
@@ -34,7 +34,7 @@ pub fn run(data: &[u8]) {
 
     // Try to parse an expression; we don't care about the result here — panics
     // and crashes are what the fuzzer should find.
-    let res = Program::parse(tokens.iter().copied());
+    let res = Program::parse(tokens.iter().copied(), &global);
     match res {
         Ok(s) => {
             s.display(&global);
