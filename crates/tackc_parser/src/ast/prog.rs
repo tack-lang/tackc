ac6f9adba93a4364b9be42797d4e89f4851b0c50
Chloe <228616821+akiramusic000@users.noreply.github.com>
2025-12-26 16:07:28 -0600

    Program parsing!

---
+++ crates/tackc_parser/src/ast/prog.rs
@@ -0,0 +1,62 @@
+use tackc_global::Global;
+use thin_vec::ThinVec;
+
+use crate::{
+    NodeId,
+    ast::{Item, Symbol},
+};
+
+pub struct Program {
+    pub mod_stmt: Option<ModStatement>,
+    pub items: ThinVec<Option<Item>>,
+}
+
+impl Program {
+    pub fn display(&self, global: &Global) -> String {
+        format!(
+            "{}\n{}",
+            self.mod_stmt.as_ref().map_or_else(
+                || String::from("<ERROR>;"),
+                |mod_stmt| mod_stmt.display(global)
+            ),
+            self.items
+                .iter()
+                .map(|opt| opt
+                    .as_ref()
+                    .map_or_else(|| String::from("<ERROR>"), |sym| sym.display(global)))
+                .collect::<Vec<_>>()
+                .join("\n")
+        )
+    }
+}
+
+pub struct ModStatement {
+    pub path: Option<Path>,
+    pub id: NodeId,
+}
+
+impl ModStatement {
+    pub fn display(&self, global: &Global) -> String {
+        format!(
+            "mod {};",
+            self.path
+                .as_ref()
+                .map_or_else(|| String::from("<ERROR>"), |path| path.display(global))
+        )
+    }
+}
+
+pub struct Path {
+    pub components: ThinVec<Option<Symbol>>,
+    pub id: NodeId,
+}
+
+impl Path {
+    pub fn display(&self, global: &Global) -> String {
+        self.components
+            .iter()
+            .map(|opt| opt.map_or("<ERROR>", |sym| sym.display(global)))
+            .collect::<Vec<_>>()
+            .join(".")
+    }
+}
